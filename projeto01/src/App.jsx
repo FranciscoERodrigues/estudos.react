e3cbef5245d7d9749d94490e89832cff4fcb5b82
Francisco Emerson Rogerio Rodrigues <163360208+FranciscoERodrigues@users.noreply.github.com>
2024-10-22 20:59:21 -0300

    Add files via upload

---
+++ projeto01/src/App.jsx
@@ -0,0 +1,11 @@
+import ListaDeTarefas from "./components/Listadetarefas";
+
+function App() {
+  return (
+    <div className="App">
+      <ListaDeTarefas />
+    </div>
+  );
+}
+
+export default App;
